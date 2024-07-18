69542eab6933f348047c54928baafc0022ae4d52
SamuelDias7 <samuelhenrique1234765@gmail.com>
2024-07-17 22:51:26 -0300

    Tornando o projeto mais dinamico

--- springWeb/src/main/resources/static/css/script.js
+++ springWeb/src/main/resources/static/css/script.js
@@ -1,4 +1,6 @@
 function metodo(numero, container){
+
+    //DOM serve pra que o javascript tenha acesso ao HTMl(acesse, manipule e interaja)
     document.addEventListener("DOMContentLoaded", function() {
 
         fetch('http://localhost:8080/exibir/' + numero)
@@ -9,7 +11,7 @@ function metodo(numero, container){
                 NovoParagrafo.textContent = data.pais;
 
                 var Capital = document.createElement("p");
-                Capital.textContent = "capital: " +data.capital;
+                Capital.textContent = "capital: "+ data.capital;
 
                 var Continente = document.createElement("p")
                 Continente.textContent = "continente: " + data.continente;
@@ -20,8 +22,8 @@ function metodo(numero, container){
                 var Presidente = document.createElement("p")
                 Presidente.textContent = "presidente: " + data.presidente;
 
-                
-                
+
+
                     var Container = document.getElementById("container-pais" + container);
                     Container.appendChild(NovoParagrafo)
                     Container.appendChild(Capital)
@@ -29,20 +31,92 @@ function metodo(numero, container){
                     Container.appendChild(Idioma)
                     Container.appendChild(Presidente)
 
+                    let insta = document.getElementById("insta")
+                    insta.innerText = "Instagram: samuel.dias7 ";
 
+                    let email = document.getElementById("email")
+                    email.innerText = "Email: dias7.sam@gmail.com";
+
+                    let github = document.getElementById("github")
+                    github.innerHTML = "GitHub: SamuelDias7";
+
+                    let barraDePesquisa = document.getElementById("pesquisar").value
             })
 
-            
             .catch(error => console.error('Erro ao buscar dados:', error));
 
 });
+
 }
 
-metodo(1,1);
-metodo(2,2);
-metodo(3,3);
-metodo(4,4);
-metodo(5,5);
-metodo(6,6);
-metodo(7,7);
-metodo(8,8);
+let numeroAleatorio = 0;
+let numeroAleatorioInteiro = 0;
+let testando = 9;
+var array_name = []
+var array_repetidos = [9]
+
+let checar = 0;
+
+function gerarNumero(){
+
+    //Gera o numero Aleatorio
+    numeroAleatorio = Math.random();
+    numeroAleatorio = numeroAleatorio * 10 + 1;
+    numeroAleatorioInteiro = parseInt(numeroAleatorio)
+    console.log("O numero alatorio é:" + numeroAleatorioInteiro)
+
+
+    console.log("Os numeros que foram encontrados são: "+ array_repetidos)
+
+    if(array_repetidos.includes(numeroAleatorioInteiro)){
+        console.log("Esta incluso");
+
+    }else{
+
+        console.log("nao esta incluso");
+        array_name.push(numeroAleatorioInteiro)
+        console.log("Numero que nao foram repetidos:" + array_name)
+    }
+
+    array_repetidos.push(numeroAleatorioInteiro);
+
+
+}
+
+//O Erro do codigo acontece quando o if aonde esta escrito "Esta incluso" entra em cena
+
+
+//Otimizar isto aqui, pois essa falta de organização esta atrapalhando a resolução de bugs
+gerarNumero();
+metodo(array_name[0],1);
+gerarNumero();
+metodo(array_name[1],2);
+gerarNumero();
+metodo(array_name[2],3);
+gerarNumero();
+metodo(array_name[3],4);
+gerarNumero();
+metodo(array_name[4],5);
+gerarNumero();
+metodo(array_name[5],6);
+gerarNumero();
+metodo(array_name[6],7);
+gerarNumero();
+metodo(array_name[7],8);
+gerarNumero();
+metodo(array_name[8],8);
+gerarNumero();
+
+function pegarDado(){
+    let campo = document.getElementById("Input-pesquisar").value
+    alert(campo)
+
+    let limpar = document.getElementById("Input-pesquisar").value = "";
+
+
+
+    /*
+    window.location.href = "http://localhost:8080/exibir/4";
+
+    */
+}
